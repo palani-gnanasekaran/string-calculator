8852527d58fa32ff269d7d39eef96b774b377804
Gnanasekaran Palanisamy <gnanasekaran.palanisamy@gmail.com>
2020-11-26 21:33:08 +0530

    Modified String calculator to handle single number input.

--- src/main/java/com/sample/StringCalculator.java
+++ src/main/java/com/sample/StringCalculator.java
@@ -1,9 +1,12 @@
 package com.sample;
 
 public class StringCalculator {
+	
 	public int add(String numbers) {
 		if( "".equals(numbers) )
 			return 0;
-		return 1;
+		else {
+			return Integer.parseInt(numbers);
+		}
 	}
 }
