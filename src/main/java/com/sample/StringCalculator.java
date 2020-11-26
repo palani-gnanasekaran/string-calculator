3fc5b172175c21e27e82d7c5b6a64ef2740e08e1
Gnanasekaran Palanisamy <gnanasekaran.palanisamy@gmail.com>
2020-11-26 21:39:40 +0530

    Modified string calculator to handle max 2 numbers addition

--- src/main/java/com/sample/StringCalculator.java
+++ src/main/java/com/sample/StringCalculator.java
@@ -2,11 +2,18 @@ package com.sample;
 
 public class StringCalculator {
 	
+	private int intValue( String value ) {
+		return Integer.parseInt(value);
+	}
 	public int add(String numbers) {
 		if( "".equals(numbers) )
 			return 0;
 		else {
-			return Integer.parseInt(numbers);
+			String[] values = numbers.split(",");
+			if(values.length == 1 ) {
+				return intValue(values[0]);
+			}
+			return intValue(values[0]) + intValue(values[1]);
 		}
 	}
 }
